cdb8ae74dfbee51b8f392c5caddf5c151db2d8e4
niwong <nick@nickwong.io>
2019-09-30 01:38:49 -0700

    demonstrate and implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
+
+// Returns a makeshift navbar to demonstrate react-router.
+function NavBar() {
+  return (
+    <div className="makeshiftNav">
+      <nav>
+        <ul>
+          <li className="navItem">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navItem">
+            <Link to="/onboarding">Sign Up</Link>
+          </li>
+          <li className="navItem">
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+
+          <NavBar/>
+
+          <Switch>
+
+            <Route path="/onboarding">
+              <img src="https://image.flaticon.com/icons/svg/1615/1615404.svg" className="App-logo" alt="logo" />
+              <p>
+                Onboarding Route
+                
+              </p>
+            </Route>
+
+            <Route path="/dashboard">
+              <img src="https://image.flaticon.com/icons/svg/2150/2150686.svg" className="App-logo" alt="logo" />
+              <p>
+                Dashboard Route
+              </p>
+            </Route> 
+
+            <Route path="/">
+              <img src="https://image.flaticon.com/icons/svg/2114/2114481.svg" className="App-logo" alt="logo" />
+              <p>
+                Root Route
+              </p>
+            </Route>
+          </Switch>
+
+          <p>This is react-router(-dom) in action! Different URL slugs 🐌 trigger different components to be rendered. <br/> Click the nav buttons to navigate through the different URLs.</p>
+
+        </header>
+      </div>
+    </Router>
   );
 }
 
