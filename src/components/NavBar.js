f16d513b06271b2c96baa8a43b87d9657095d322
niwong <nick@nickwong.io>
2019-10-19 11:50:26 -0700

    Change NavBar to a class component and add current user's information based off of a hard-coded id.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import '../styles/NavBar.css';
+import { getRecordWithPromise } from '../request'
 
 // TODO: Beneath the UL we can add a ProfilePicture component that displays current user info 
 
 
-export default function NavBar() {
-  return (
-    <div className="navBarCont">
-      <nav>
-        <ul>
-          <li className="navItem">
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li className="navItem">
-            <Link to="/finances">My Finances</Link>
-          </li>
-          <li className="navItem">
-            <Link to="/community">Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  )
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    }
+  }
+
+  componentDidMount() {
+    // hard-coded my id
+    const id = 'recfnsL4HDoNHril6';
+    let record = getRecordWithPromise('Person', id).then((payload) => {
+      let { "Name": name } = payload.record
+      this.setState({
+        name: name
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div className="navBarCont">
+        <nav>
+          <ul>
+            <li className="navItem">
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li className="navItem">
+              <Link to="/finances">My Finances</Link>
+            </li>
+            <li className="navItem">
+              <Link to="/community">Community</Link>
+            </li>
+            <li><span>{this.state.name}</span></li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
 }
