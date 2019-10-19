ddbc5f8888830dc3b9b078256073f79d0651c61d
niwong <nick@nickwong.io>
2019-10-19 13:34:38 -0700

    Remove testing functions

--- src/App.js
+++ src/App.js
@@ -5,31 +5,6 @@ import { getRecord, createPerson, updatePerson, getRecordFromAttribute } from '.
 import NavBar from './components/NavBar'
 import GeneralOwnerDashboard from './components/GeneralOwnerDashboard'
 
-// This probably isn't proper react style, but it works.
-function handleButton() {
-  // var record = getRecord('Person', 'recfnsL4HDoNHril6');
-  var record = getRecordFromAttribute('Person', 'Email', 'ashley@pppower.io')
-
-  var person = {
-    "fields": {
-      "Email": "iris@pppower.io",
-      "Phone Number": "(504) 343-6954",
-      "Tags": 12,
-      "Name": "Iris Hou"
-    }
-  }
-
-  var updatedPerson = {
-    "id": "recfnsL4HDoNHril6",
-    "fields": {
-      "Name": "Nicholas Wong",
-      "Phone Number": "(808) 343-6954"
-    }
-  }
-
-  // updatePerson(updatedPerson);
-}
-
 function App() {
   return (
     <Router>
@@ -37,26 +12,22 @@ function App() {
         <NavBar/>
 
         <Switch>
-
           <Route exact path="/">
-            <img src="https://image.flaticon.com/icons/svg/2114/2114481.svg" className="App-logo" alt="logo" />
-            <p>
-              Root Route
+            <p style={{color: 'white', margin: '30px'}}>
+              Root Route - A react component or "screen" will go here.
             </p>
-            <button onClick={handleButton}>Make request</button>
           </Route>
 
           <Route path="/onboarding">
-            <img src="https://image.flaticon.com/icons/svg/1615/1615404.svg" className="App-logo" alt="logo" />
             <p>
-              Onboarding Route
+              Onboarding Route - Asli's onboarding screen will go here.
             </p>
           </Route>
 
-          <Route path="/dashboard" render={(props) => <GeneralOwnerDashboard {...props} />} />   
+          <Route path="/dashboard" render={(props) => <GeneralOwnerDashboard {...props} />} /> 
 
           <Route>
-            Not Found - 404
+            <p style={{color: 'white', margin: '30px'}}>Not Found - 404</p>
           </Route>
         </Switch>
 
