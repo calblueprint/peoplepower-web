4a79455b62b7660ab464bd645fb195d795876728
niwong <nick@nickwong.io>
2019-10-20 20:29:20 -0700

    Write route for user profile page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NavBar from './components/NavBar'
 import GeneralOwnerDashboard from './screens/general/GeneralOwnerDashboard'
 import Onboarding from './screens/onboarding/Onboarding'
 import Login from './screens/auth/Login'
-import UserProfilePage from './components/UserProfilePage'
+import UserProfilePage from './screens/general/UserProfilePage'
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/" component={Login}/>
           <Route path="/onboarding" component={Onboarding}/>
           <Route path="/dashboard" component={GeneralOwnerDashboard}/>
-          <Route path="/dashboard" component={UserProfilePage}/> 
+          <Route path="/profile/:id" component={UserProfilePage}/> 
           <Route>
             <p style={{color: 'white', margin: '30px'}}>Not Found - 404</p>
           </Route>
