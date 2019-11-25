af1b913dab875d784445365d95f69efa39c262cf
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2019-11-24 20:04:55 -0800

    fix bug

--- src/screens/onboarding/MapView.js
+++ src/screens/onboarding/MapView.js
@@ -2,73 +2,72 @@ import React from 'react';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 import keys from '../../lib/api_key';
 
-class MapView extends React.Component {
-  render() {
-    const {
-      google,
-      markers,
-      displayGroup,
-      view,
-      changeSelectedGroup
-    } = this.props;
-    return (
-      <div
-        className="list "
-        style={{ display: view === 'map' ? 'flex' : 'none' }}
+function MapView(props) {
+  const {
+    google,
+    markers,
+    displayGroup,
+    view,
+    changeDisplayedGroup,
+    changeSelectedGroup
+  } = props;
+  return (
+    <div
+      className="list "
+      style={{ display: view === 'map' ? 'flex' : 'none' }}
+    >
+      <Map
+        google={google}
+        zoom={10}
+        style={{
+          width: '75%',
+          height: '75%',
+          borderRadius: 10,
+          zIndex: 0
+        }}
+        // className="mapWindow"
+        initialCenter={{
+          lat: 37.8719,
+          lng: -122.2585
+        }}
+        zoomControl
+        zoomControlOptions={{
+          position: google.maps.ControlPosition.LEFT_CENTER
+        }}
+        mapTypeControl={false}
+        scaleControl={false}
+        streetViewControl={false}
+        rotateControl={false}
+        fullscreenControl
       >
-        <Map
-          google={google}
-          zoom={10}
-          style={{
-            width: '75%',
-            height: '75%',
-            borderRadius: 10,
-            zIndex: 0
-          }}
-          // className="mapWindow"
-          initialCenter={{
-            lat: 37.8719,
-            lng: -122.2585
-          }}
-          zoomControl
-          zoomControlOptions={{
-            position: google.maps.ControlPosition.LEFT_CENTER
-          }}
-          mapTypeControl={false}
-          scaleControl={false}
-          streetViewControl={false}
-          rotateControl={false}
-          fullscreenControl
-        >
-          {markers.map((marker, index) => (
-            <Marker
-              name={marker.name}
-              position={{ lat: marker.lat, lng: marker.lng }}
-              onClick={() => this.changeDisplayedGroup(index)}
-            />
-          ))}
-        </Map>
-        <div className="infoBox">
-          <div style={{ margin: '20px' }}>
-            <div style={{ fontSize: '24px', paddingBottom: '10px' }}>
-              {markers[displayGroup].name}
-            </div>
-            <div style={{ fontSize: '14px', paddingBottom: '20px' }}>
-              {markers[displayGroup].description}
-            </div>
-            <button
-              type="button"
-              style={{ bottom: 0 }}
-              onClick={() => changeSelectedGroup(markers[displayGroup])}
-              name="projectGroup"
-            >
-              Select
-            </button>
+        {markers.map((marker, index) => (
+          <Marker
+            name={marker.name}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => changeDisplayedGroup(index)}
+          />
+        ))}
+      </Map>
+      <div className="infoBox">
+        <div style={{ margin: '20px' }}>
+          <div style={{ fontSize: '24px', paddingBottom: '10px' }}>
+            {markers[displayGroup].name}
           </div>
+          <div style={{ fontSize: '14px', paddingBottom: '20px' }}>
+            {markers[displayGroup].description}
+          </div>
+          <button
+            type="button"
+            style={{ bottom: 0 }}
+            onClick={() => changeSelectedGroup(markers[displayGroup])}
+            name="projectGroup"
+          >
+            Select
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default GoogleApiWrapper({
