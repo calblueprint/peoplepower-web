820cbceb79ccddde96697f96befe6f54efcb60ec
Fang Shuo <dfs.arthur@berkeley.edu>
2019-11-26 14:10:59 -0800

    Add  function to create a user after the end of 'signup' phase (#29)
    * add  function to create a user after the end of 'signup' phase
    
    * add  function to create a user after the end of 'signup' phase

--- src/screens/onboarding/Onboarding.js
+++ src/screens/onboarding/Onboarding.js
@@ -65,7 +65,7 @@ class Onboarding extends React.Component {
         ccstate: '',
         cczipcode: ''
       },
-      step: 3
+      step: 1
     };
     this.handleChange = this.handleChange.bind(this);
     this.callBackBylawValidation = this.callBackBylawValidation.bind(this);
