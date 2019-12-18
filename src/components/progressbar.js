66f44075742f411733160ea3327a79a23b704e9b
Ashley <ashley.nguyen27@berkeley.edu>
2019-12-18 11:17:47 -0800

    progress bar using svg instead of ul, li tag

--- src/components/progressbar.js
+++ src/components/progressbar.js
@@ -13,16 +13,88 @@ function colorStep(activeStep, step) {
 export default function ProgressBar(step) {
   return (
     <div className="progress-bar-container">
-      <ul className="progressbar">
-        <li className={`${colorStep(step, 2)} progress-bar-text`}>Contact</li>
-        <li className={`${colorStep(step, 3)} progress-bar-text`}>
-          Project Group
-        </li>
-        <li className={`${colorStep(step, 4)} progress-bar-text`}>
-          Owner Agreement
-        </li>
-        <li className={`${colorStep(step, 5)} progress-bar-text`}>Payment</li>
-      </ul>
+      <div className="progressbar">
+        <div className={`${colorStep(step, 2)} progress-step`}>
+          <svg height="4rem" width="4rem">
+            <circle
+              className="progress-bar-circle"
+              cx="2rem"
+              cy="2rem"
+              strokeLocation="inside"
+            />
+            <polyline
+              className="progress-bar-checkmark"
+              fill="none"
+              stroke="white"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              points="41,27 30,40 22,34 "
+            />
+          </svg>
+          <div className="progressbar-step-text">Contact</div>
+        </div>
+        <div className={`${colorStep(step, 3)} progress-step`}>
+          <svg height="4rem" width="4rem">
+            <circle
+              className="progress-bar-circle"
+              cx="2rem"
+              cy="2rem"
+              strokeLocation="inside"
+            />
+            <polyline
+              className="progress-bar-checkmark"
+              fill="none"
+              stroke="white"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              points="41,27 30,40 22,34 "
+            />
+          </svg>
+          <div className="progressbar-step-text">Project Groups</div>
+        </div>
+        <div className={`${colorStep(step, 4)} progress-step`}>
+          <svg height="4rem" width="4rem">
+            <circle
+              className="progress-bar-circle"
+              cx="2rem"
+              cy="2rem"
+              strokeLocation="inside"
+            />
+            <polyline
+              className="progress-bar-checkmark"
+              fill="none"
+              stroke="white"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              points="41,27 30,40 22,34 "
+            />
+          </svg>
+          <div className="progressbar-step-text">Owner Agreement</div>
+        </div>
+        <div className={`${colorStep(step, 5)} progress-step`}>
+          <svg height="4rem" width="4rem">
+            <circle
+              className="progress-bar-circle"
+              cx="2rem"
+              cy="2rem"
+              strokeLocation="inside"
+            />
+            <polyline
+              className="progress-bar-checkmark"
+              fill="none"
+              stroke="white"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              points="41,27 30,40 22,34 "
+            />
+          </svg>
+          <div className="progressbar-step-text">Payment</div>
+        </div>
+      </div>
     </div>
   );
 }
