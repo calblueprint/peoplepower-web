2622202ce5cce0e647f27684ba474c9b8592ea01
Fang Shuo <dfs.arthur@berkeley.edu>
2020-03-05 16:29:39 -0800

    Incorporated react-modal (#90)
    * re-implement modal with react-modal
    * rename PpModal -> PPModal

--- src/screens/workshop/ModalDemo.js
+++ src/screens/workshop/ModalDemo.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import Modal from '../../components/Modal';
+import PPModal from '../../components/PPModal';
+import '../../styles/Modal.css';
 
 export default class ModalDemo extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      showModal: true
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  /* open/close modal logic */
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
   }
 
   render() {
+    const { showModal } = this.state;
     return (
       <div>
-        <Modal
+        <PPModal
           // header="Optional Heading"
           body="Hello"
           actionName="Close"
+          showModal={showModal}
+          handleCloseModal={this.handleCloseModal}
         />
       </div>
     );
