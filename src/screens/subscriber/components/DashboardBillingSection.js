605ce8db49c226b2a86d6b21b5efdb5ae1c4425f
Ashley Nguyen <ashley.nguyen27@yahoo.com>
2020-03-16 11:34:04 -0700

    Ashley/subscriber dashboard (#99)
    * skeleton for subscriber dashboard
    
    * billing
    
    * final billing section
    
    * charts
    
    * final announcement styling
    
    * disabled button and tabs
    
    * lint
    
    * Subscriber Dashboard Final
    
    * redirects to /billing bug
    
    * remove gradle files
    
    * remove gradlew.bat
    
    * gitignore
    
    * fixes from comments
    
    * updated the change tab function

---
+++ src/screens/subscriber/components/DashboardBillingSection.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ReactTable from 'react-table-v6';
+import { centsToDollars } from '../../../lib/subscriberUtils';
+import RightArrow from '../../../assets/right_arrow.png';
+
+export default class DashboardBilling extends React.PureComponent {
+  render() {
+    const { totalBalance, data } = this.props;
+    return (
+      <div>
+        <div className="subscriber-section-header">
+          <div className="subscriber-header">Billing Summary</div>
+          <Link to="/billing">
+            <img src={RightArrow} alt="right arrow" />
+          </Link>
+        </div>
+        <div className="subscriber-section-body">
+          <div>
+            <div className="subscriber-billing-container">
+              <div className="subscriber-billing-current-container">
+                <div className="subscriber-billing-header">Current Balance</div>
+                <h3 className="subscriber-billing-balance">
+                  ${centsToDollars(totalBalance)}
+                </h3>
+                <div>
+                  <button
+                    type="button"
+                    className={`subscriber-billing-make-payment-button ${
+                      totalBalance === 0 ? 'disabled' : ''
+                    }`}
+                    disabled={totalBalance === 0}
+                  >
+                    <Link to="/billing" className="subscriber-link-text-white">
+                      Make Payment
+                    </Link>
+                  </button>
+                </div>
+              </div>
+              <div className="subscriber-billing-recent-container">
+                <div className="subscriber-billing-header-inline">
+                  <div className="subscriber-billing-header">
+                    Recent Transactions
+                  </div>
+                  <div>
+                    <button
+                      type="button"
+                      className="subscriber-billing-view-all-button"
+                    >
+                      <Link
+                        to="/billing?view=all"
+                        className="subscriber-link-text-white "
+                      >
+                        View All
+                      </Link>
+                    </button>
+                  </div>
+                </div>
+                <ReactTable
+                  data={data}
+                  columns={[
+                    {
+                      id: 'statementDate',
+                      accessor: d => (
+                        <div className="subscriber-billing-recent-row ">
+                          {d.statementDate}
+                        </div>
+                      )
+                      // width: 100
+                    },
+                    {
+                      id: 'description',
+                      accessor: d => (
+                        <div className="subscriber-billing-recent-row ">
+                          <b>{d.description}</b>
+                        </div>
+                      ),
+                      width: 200
+                    },
+                    {
+                      id: 'amtDue',
+                      accessor: d => (
+                        <div className="subscriber-billing-recent-row ">
+                          {d.amtDue ? `+${d.amtDue}` : `-${d.payment}`}
+                        </div>
+                      )
+                      // width: 150
+                    },
+                    {
+                      id: 'status',
+                      accessor: d => (
+                        <div className="subscriber-billing-recent-row ">
+                          {d.status}
+                        </div>
+                      )
+                      // width: 100
+                    }
+                  ]}
+                  getTdProps={() => ({
+                    style: { border: 'none' }
+                  })}
+                  defaultPageSize={2}
+                  className="subscriber-billing-recent-table"
+                  showPagination={false}
+                  getTrGroupProps={() => {
+                    return {
+                      style: {
+                        border: 'none'
+                      }
+                    };
+                  }}
+                  getTheadProps={() => {
+                    return {
+                      style: {
+                        boxShadow: 'none'
+                      }
+                    };
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
