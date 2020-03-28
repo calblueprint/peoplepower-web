153cb3f7b9b38612f94c85b9aa835d1cb4b6373e
cindyzhang12 <61183011+cindyzhang12@users.noreply.github.com>
2020-03-28 00:05:48 -0400

    Investments Webpage Box Content and Modal (#104)
    * investments webpage box contents
    
    * added modal popup
    
    * updated css div names
    
    * renamed more divs
    
    * changes from fang's review
    
    * aivants comments
    
    * updated progressbar variable names

--- src/screens/general/Investment.js
+++ src/screens/general/Investment.js
@@ -1,17 +1,81 @@
 import React from 'react';
+import 'react-circular-progressbar/dist/styles.css';
+import { connect } from 'react-redux';
+import SharesProgressBar from './components/SharesProgressBar';
+import DividendsPreferencesModal from './components/DividendsPreferencesModal';
+import { updateOwner } from '../../lib/airtable/request';
+import { refreshUserData } from '../../lib/userDataUtils';
 import '../../styles/Investments.css';
+import GreenCheck from '../../assets/green_check.png';
+import RedX from '../../assets/red_x.png';
 
 class Investment extends React.PureComponent {
+  submitPreference = async newIsReceivingDividends => {
+    const { owner } = this.props;
+    await updateOwner(owner.id, {
+      isReceivingDividends: newIsReceivingDividends
+    });
+    await refreshUserData(owner.id);
+  };
+
   render() {
+    const { owner } = this.props;
+
     return (
       <div className="dashboard">
         <div className="mainheader">
           <h1>My Investment</h1>
           <div className="columnformat">
-            <div className="left-content">
+            <div className="investment-and-transactions-content">
               <h2>My Investment</h2>
-              <div className="investment-box-1">investments box 1 test</div>
-              <div className="investment-box-2">investments box 2 test</div>
+              <div className="investments-box-shares">
+                <SharesProgressBar numberOfShares={owner.numberOfShares} />
+                <div className="box-text">
+                  <h5>
+                    You currently own {owner.numberOfShares} out of 10 possible
+                    shares
+                  </h5>
+                  <br />
+                  <h4>${owner.numberOfShares * 100}.00</h4>
+                </div>
+                <div className="investments-buttons">
+                  <div className="investments-buy-shares-button">
+                    <a className="button" href="/investment">
+                      <span>Buy Shares</span>
+                    </a>
+                  </div>
+                  <div className="investments-dividend">Divest</div>
+                </div>
+              </div>
+              <div className="investments-box-dividends">
+                <div className="dividends-preferences-box">
+                  <h4>Dividend Preferences</h4>
+                  <div className="status">
+                    <img
+                      className="green-check"
+                      src={
+                        owner.isReceivingDividends === true ? GreenCheck : RedX
+                      }
+                      alt={
+                        owner.isReceivingDividends === true
+                          ? 'Green Check'
+                          : 'Red X'
+                      }
+                    />
+                    <span>
+                      {owner.isReceivingDividends === true ? (
+                        <h6>Currently receiving dividends</h6>
+                      ) : (
+                        <h6>Not receiving dividends</h6>
+                      )}
+                    </span>
+                  </div>
+                </div>
+                <DividendsPreferencesModal
+                  newIsReceivingDividends={owner.newIsReceivingDividends}
+                  onClickSavePreferences={this.submitPreference}
+                />
+              </div>
               <h2>Transactions</h2>
               <div className="transactions-box">transactions box test</div>
             </div>
@@ -26,4 +90,8 @@ class Investment extends React.PureComponent {
   }
 }
 
-export default Investment;
+const mapStateToProps = state => ({
+  owner: state.userData.owner
+});
+
+export default connect(mapStateToProps)(Investment);
