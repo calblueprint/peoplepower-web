7570d3d06834d4dece87d51e23f2a7a4f3115ea1
Cindy Zhang <cindyzhang0718@gmail.com>
2020-04-09 01:00:05 -0400

    transactions list

---
+++ src/screens/general/components/TransactionsList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import moment from 'moment';
+
+class TransactionList extends React.PureComponent {
+  render() {
+    const { payments } = this.props;
+    const filteredList = payments.filter(
+      transaction => transaction.type !== 'Bill Payment'
+    );
+    const list = filteredList.map(payment => {
+      const { type, amount, dateCreated, id } = payment;
+      const quantity = amount / 100;
+      const dateFormatted = moment(dateCreated).format('MMMM D, YYYY');
+
+      return (
+        <div key={id} className="transaction-card">
+          <div className="transaction-card-heading">
+            <h4>{type === 'Buy Shares' ? 'Share' : type}</h4>
+            <h5>
+              {type === 'Buy Shares' ? '-' : '+'}${amount}.00
+            </h5>
+          </div>
+          <div className="transaction-card-details">
+            {type === 'Dividend' ? null : <p>Quantity: {quantity}</p>}
+            <h6>{dateFormatted}</h6>
+            <hr />
+          </div>
+        </div>
+      );
+    });
+
+    return <div style={{ overflow: 'auto' }}>{list}</div>;
+  }
+}
+
+export default TransactionList;
