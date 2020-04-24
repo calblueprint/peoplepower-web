1ffa8da75a5564a01997ffdb9c194e5a27cf6396
arpanpal <arpan.pal1234@gmail.com>
2020-04-23 23:58:36 -0500

    install sentry

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import { store, persistor } from './lib/redux/store';
+
+Sentry.init({
+  dsn:
+    'https://b7796c6c9c1a4a41a8d40a9a0ae18596@o306199.ingest.sentry.io/5211924'
+});
 
 ReactDOM.render(
   <Provider store={store}>
