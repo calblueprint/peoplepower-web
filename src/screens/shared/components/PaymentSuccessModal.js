f63a5d6bd2fd92e776c04ac79032a7d8a6a51010
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2020-04-25 22:32:14 +0800

    move colors to constants

--- src/screens/shared/components/PaymentSuccessModal.js
+++ src/screens/shared/components/PaymentSuccessModal.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../../styles/PaymentSuccessModal.css';
+import Colors from '../../../colors';
+
+const { PP_PINK } = Colors;
 
 class PaymentSuccessModal extends React.PureComponent {
   render() {
@@ -17,16 +20,10 @@ class PaymentSuccessModal extends React.PureComponent {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <circle
-            cx="41.5"
-            cy="41.5"
-            r="39"
-            stroke="#CD6795"
-            strokeWidth="5"
-          />
+          <circle cx="41.5" cy="41.5" r="39" stroke={PP_PINK} strokeWidth="5" />
           <path
             d="M26.7522 41.7906C25.7674 40.8229 24.1845 40.8367 23.2168 41.8216C22.2491 42.8064 22.263 44.3893 23.2478 45.357L26.7522 41.7906ZM34.593 53L32.8408 54.7832L34.593 56.5049L36.3452 54.7832L34.593 53ZM59.7522 31.7832C60.737 30.8155 60.7509 29.2326 59.7832 28.2478C58.8155 27.263 57.2326 27.2491 56.2478 28.2168L59.7522 31.7832ZM23.2478 45.357L32.8408 54.7832L36.3452 51.2168L26.7522 41.7906L23.2478 45.357ZM36.3452 54.7832L59.7522 31.7832L56.2478 28.2168L32.8408 51.2168L36.3452 54.7832Z"
-            fill="#CD6795"
+            fill={PP_PINK}
           />
         </svg>
 
