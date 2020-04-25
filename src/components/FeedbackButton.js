3ab95e684f67a4cb3d43277281c9cf2f3179c09e
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2020-04-25 19:50:44 +0800

    working animation

--- src/components/FeedbackButton.js
+++ src/components/FeedbackButton.js
@@ -8,16 +8,41 @@ class FeedbackButton extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      buttonText: '?'
+      buttonText: '?',
+      showText: false
     };
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+
+  onMouseEnter() {
+    this.setState({
+      showText: true,
+      buttonText: ''
+    });
+    setTimeout(() => {
+      const { showText } = this.state;
+      if (showText) {
+        this.setState({ buttonText: 'Report an Issue' });
+      }
+    }, 100);
+    // if (showText) {
+    //   this.setState({ buttonText: 'Report an Issue' });
+    // }
+    // setTimeout(() => this.setState({ buttonText: 'Report an Issue' }), 90);
+  }
+
+  onMouseLeave() {
+    this.setState({ showText: false });
+    this.setState({ buttonText: '?' });
   }
 
   render() {
     const { buttonText } = this.state;
     return (
       <button
-        onMouseEnter={() => this.setState({ buttonText: 'Report an Issue' })}
-        onMouseLeave={() => this.setState({ buttonText: '?' })}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
         type="button"
         className="feedback-button-fixed"
         target="_blank"
