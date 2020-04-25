5738f4b56c01d61f2e0c075ca7cb0e12e3932fa5
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2020-04-25 11:27:58 +0800

    add animations

--- src/components/FeedbackButton.js
+++ src/components/FeedbackButton.js
@@ -4,18 +4,32 @@ import Constants from '../constants';
 
 const { BUG_REPORT_URL } = Constants;
 
-export default function FeedbackButton() {
-  return (
-    <button
-      type="button"
-      className="feedback-button-fixed"
-      target="_blank"
-      onClick={e => {
-        e.preventDefault();
-        window.open(BUG_REPORT_URL);
-      }}
-    >
-      ?
-    </button>
-  );
+class FeedbackButton extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonText: '?'
+    };
+  }
+
+  render() {
+    const { buttonText } = this.state;
+    return (
+      <button
+        onMouseEnter={() => this.setState({ buttonText: 'Report an Issue' })}
+        onMouseLeave={() => this.setState({ buttonText: '?' })}
+        type="button"
+        className="feedback-button-fixed"
+        target="_blank"
+        onClick={e => {
+          e.preventDefault();
+          window.open(BUG_REPORT_URL);
+        }}
+      >
+        {buttonText}
+      </button>
+    );
+  }
 }
+
+export default FeedbackButton;
