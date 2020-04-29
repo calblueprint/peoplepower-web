9150a29f64bab2885be8fd196d5d1a4cbedd352d
Cindy Zhang <61183011+cindyzhang12@users.noreply.github.com>
2020-04-29 01:44:19 -0400

    Onboarding, Payment: various bug Fixes (#162)
    Co-authored-by: Fang Shuo Deng <dfs.arthur@berkeley.edu>

--- src/screens/onboarding/steps/BasicInfoStep.js
+++ src/screens/onboarding/steps/BasicInfoStep.js
@@ -16,7 +16,7 @@ class BasicInfoStep extends React.PureComponent {
       toggleValidColor
     } = this.props;
     return (
-      <form className="center card flex onboarding-col">
+      <form className="card flex onboarding-col onboarding-first-step-margin">
         <div className=" ">
           <div className="header ">
             Be a part of the movement for equity and clean energy.
@@ -109,7 +109,7 @@ class BasicInfoStep extends React.PureComponent {
         <div className="v-center">
           <button
             type="button"
-            className="btn btn--rounded btn--pink btn--size12 getstarted-button"
+            className="btn btn--rounded btn--pink btn--size12 get-started-button"
             onClick={onSubmit}
           >
             Get Started
