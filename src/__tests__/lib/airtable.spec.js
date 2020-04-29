876c4106daa0c496ef02e708611d937328771914
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2020-04-29 16:44:02 +0800

    rm tests

--- src/__tests__/lib/airtable.spec.js
+++ src/__tests__/lib/airtable.spec.js
@@ -1,116 +1,117 @@
+// TODO: tests after airlock
 /* eslint no-restricted-imports: 0 */
 
-import {
-  createRecord,
-  getAllRecords,
-  getRecordById,
-  getRecordsByAttribute,
-  updateRecord,
-  deleteRecord
-} from '../../lib/airtable/airtable';
+// import {
+//   createRecord,
+//   getAllRecords,
+//   getRecordById,
+//   getRecordsByAttribute,
+//   updateRecord,
+//   deleteRecord
+// } from '../../lib/airtable/airtable';
 
-const RECORD_NOT_FOUND_ERR = 'Record not found';
-const TEST_TABLE = 'Test (Development)';
-const TEST_FIELD = 'Tag';
-const TEST_TAG = '7_NuQ6a?';
-const testId = 'rechutWysjm8RbJkq';
+// const RECORD_NOT_FOUND_ERR = 'Record not found';
+// const TEST_TABLE = 'Test (Development)';
+// const TEST_FIELD = 'Tag';
+// const TEST_TAG = '7_NuQ6a?';
+// const testId = 'rechutWysjm8RbJkq';
 
-// the structures below are
-// copies of records in Airtable.
-// If different, Airtable is source of truth
-const expectedTestResult = {
-  id: 'rechutWysjm8RbJkq',
-  name: 'TEST READ RECORD',
-  tag: TEST_TAG
-};
+// // the structures below are
+// // copies of records in Airtable.
+// // If different, Airtable is source of truth
+// const expectedTestResult = {
+//   id: 'rechutWysjm8RbJkq',
+//   name: 'TEST READ RECORD',
+//   tag: TEST_TAG
+// };
 
-const NUM_ENTRIES = 1;
-const expectedTestResultsArr = [expectedTestResult];
+// const NUM_ENTRIES = 1;
+// const expectedTestResultsArr = [expectedTestResult];
 
-const testNewRecord = {
-  name: 'TEST NEW RECORD',
-  tag: '83h)%uG9'
-};
+// const testNewRecord = {
+//   name: 'TEST NEW RECORD',
+//   tag: '83h)%uG9'
+// };
 
-describe('consistency of test data', () => {
-  test('expect true', async () => {
-    expect(expectedTestResultsArr.length).toBe(NUM_ENTRIES);
-  });
-});
+// describe('consistency of test data', () => {
+//   test('expect true', async () => {
+//     expect(expectedTestResultsArr.length).toBe(NUM_ENTRIES);
+//   });
+// });
 
-// Calls getAllRecords on the TEST_TABLE
-// verifies that NUM_ENTRIES records are returned
-// TODO: ensure the values in the array come back in the exact format we expect
-describe('getAllRecords function', () => {
-  test('expect resolve', async () => {
-    const res = await getAllRecords(TEST_TABLE);
-    expect(res.length).toBe(NUM_ENTRIES);
-    expectedTestResultsArr.forEach((expectedRes, i) => {
-      expect(res[i]).toStrictEqual(expectedRes);
-    });
-  });
-});
+// // Calls getAllRecords on the TEST_TABLE
+// // verifies that NUM_ENTRIES records are returned
+// // TODO: ensure the values in the array come back in the exact format we expect
+// describe('getAllRecords function', () => {
+//   test('expect resolve', async () => {
+//     const res = await getAllRecords(TEST_TABLE);
+//     expect(res.length).toBe(NUM_ENTRIES);
+//     expectedTestResultsArr.forEach((expectedRes, i) => {
+//       expect(res[i]).toStrictEqual(expectedRes);
+//     });
+//   });
+// });
 
-// Calls getRecordsByAttribute on the TEST_TABLE, TEST_FIELD, and TEST_TAG
-// verifies that getRecordsByAttribute returns the right record
-describe('getRecordsByAttribute function', () => {
-  test('expect resolve', async () => {
-    const res = await getRecordsByAttribute(TEST_TABLE, TEST_FIELD, TEST_TAG);
-    expect(res).toStrictEqual(expectedTestResultsArr);
-  });
-});
+// // Calls getRecordsByAttribute on the TEST_TABLE, TEST_FIELD, and TEST_TAG
+// // verifies that getRecordsByAttribute returns the right record
+// describe('getRecordsByAttribute function', () => {
+//   test('expect resolve', async () => {
+//     const res = await getRecordsByAttribute(TEST_TABLE, TEST_FIELD, TEST_TAG);
+//     expect(res).toStrictEqual(expectedTestResultsArr);
+//   });
+// });
 
-// Calls getRecordById on the TEST_TABLE and testId
-// verifies that the getRecordById returns the right record
-describe('getRecordById function', () => {
-  test('expect resolve', async () => {
-    const res = await getRecordById(TEST_TABLE, testId);
-    expect(res).toStrictEqual(expectedTestResult);
-  });
-});
+// // Calls getRecordById on the TEST_TABLE and testId
+// // verifies that the getRecordById returns the right record
+// describe('getRecordById function', () => {
+//   test('expect resolve', async () => {
+//     const res = await getRecordById(TEST_TABLE, testId);
+//     expect(res).toStrictEqual(expectedTestResult);
+//   });
+// });
 
-// Calls createRecord on the TEST_TABLE and testNewRecord. Then, calls deleteRecord on
-// the TEST_TABLE and id
-// verifies that the id is returned and res is equal to {}
+// // Calls createRecord on the TEST_TABLE and testNewRecord. Then, calls deleteRecord on
+// // the TEST_TABLE and id
+// // verifies that the id is returned and res is equal to {}
 
-// This test must run after the getRecordById test because it depends on getRecordById
-// working correctly
-describe('createRecord/updateRecord/deleteRecord functions', () => {
-  test('expect resolve', async () => {
-    jest.setTimeout(10000);
-    let id;
-    let res;
-    // create record
-    id = await createRecord(TEST_TABLE, testNewRecord);
-    expect(id).not.toBe(null);
+// // This test must run after the getRecordById test because it depends on getRecordById
+// // working correctly
+// describe('createRecord/updateRecord/deleteRecord functions', () => {
+//   test('expect resolve', async () => {
+//     jest.setTimeout(10000);
+//     let id;
+//     let res;
+//     // create record
+//     id = await createRecord(TEST_TABLE, testNewRecord);
+//     expect(id).not.toBe(null);
 
-    // verifies that the right record has been created
-    res = await getRecordById(TEST_TABLE, id);
-    expect(res.name).toStrictEqual(testNewRecord.name);
-    expect(res.tag).toStrictEqual(testNewRecord.tag);
+//     // verifies that the right record has been created
+//     res = await getRecordById(TEST_TABLE, id);
+//     expect(res.name).toStrictEqual(testNewRecord.name);
+//     expect(res.tag).toStrictEqual(testNewRecord.tag);
 
-    // update record
-    const testUpdateRecord = {
-      name: testNewRecord.name,
-      tag: testNewRecord.tag
-    };
-    id = await updateRecord(TEST_TABLE, id, testUpdateRecord);
-    expect(id).toStrictEqual(id);
+//     // update record
+//     const testUpdateRecord = {
+//       name: testNewRecord.name,
+//       tag: testNewRecord.tag
+//     };
+//     id = await updateRecord(TEST_TABLE, id, testUpdateRecord);
+//     expect(id).toStrictEqual(id);
 
-    // verifies that the right record has been updated
-    res = await getRecordById(TEST_TABLE, id);
-    expect(res.name).toStrictEqual(testNewRecord.name);
-    expect(res.tag).toStrictEqual(testNewRecord.tag);
+//     // verifies that the right record has been updated
+//     res = await getRecordById(TEST_TABLE, id);
+//     expect(res.name).toStrictEqual(testNewRecord.name);
+//     expect(res.tag).toStrictEqual(testNewRecord.tag);
 
-    // delete record
-    res = await deleteRecord(TEST_TABLE, id);
-    expect(res).toStrictEqual({});
+//     // delete record
+//     res = await deleteRecord(TEST_TABLE, id);
+//     expect(res).toStrictEqual({});
 
-    try {
-      res = await getRecordById(TEST_TABLE, id);
-      throw new Error('Deleted record should not exist');
-    } catch (err) {
-      expect(err.message).toBe(RECORD_NOT_FOUND_ERR);
-    }
-  });
-});
+//     try {
+//       res = await getRecordById(TEST_TABLE, id);
+//       throw new Error('Deleted record should not exist');
+//     } catch (err) {
+//       expect(err.message).toBe(RECORD_NOT_FOUND_ERR);
+//     }
+//   });
+// });
--- src/__tests__/lib/request.spec.js
+++ src/__tests__/lib/request.spec.js
@@ -1,177 +1,178 @@
-import {
-  getAllProjectGroups,
-  getAnnouncementsByProjectGroupId,
-  getOwnerById,
-  getProjectGroupById,
-  // getSubscriberBillById,
-  getSolarProjectById,
-  // updateSubscriberBill,
-  getOwnersByEmail,
-  updateOwner,
-  createOwner,
-  deleteOwner
-} from '../../lib/airtable/request';
+// TODO: tests after airlock
+// import {
+//   getAllProjectGroups,
+//   getAnnouncementsByProjectGroupId,
+//   getOwnerById,
+//   getProjectGroupById,
+//   // getSubscriberBillById,
+//   getSolarProjectById,
+//   // updateSubscriberBill,
+//   getOwnersByEmail,
+//   updateOwner,
+//   createOwner,
+//   deleteOwner
+// } from '../../lib/airtable/request';
 
-const testOwner = 'recG1RskfrRXd1RgS';
-const testProjectGroup = 'recxJehaSlKk8IiS9';
-const testSubscriberBill = 'recBd7TIFJfjh2k7U';
-const testSolarProject = 'rec9BemtHIasDI2aQ';
-const testEmail = 'sample@peoplepower.com';
+// const testOwner = 'recG1RskfrRXd1RgS';
+// const testProjectGroup = 'recxJehaSlKk8IiS9';
+// const testSubscriberBill = 'recBd7TIFJfjh2k7U';
+// const testSolarProject = 'rec9BemtHIasDI2aQ';
+// const testEmail = 'sample@peoplepower.com';
 
-const billFieldToModify = 'balance';
-const newBalance = 1999;
-// const oldBalance = 1996;
-const testNewSubscriberBill = {};
-testNewSubscriberBill[billFieldToModify] = newBalance;
+// const billFieldToModify = 'balance';
+// const newBalance = 1999;
+// // const oldBalance = 1996;
+// const testNewSubscriberBill = {};
+// testNewSubscriberBill[billFieldToModify] = newBalance;
 
-const ownerFieldToModify = 'numberOfShares';
-const newNumberOfShares = 99;
-const oldNumberOfShares = 1;
-const testNewOwner = {};
-testNewOwner[ownerFieldToModify] = newNumberOfShares;
+// const ownerFieldToModify = 'numberOfShares';
+// const newNumberOfShares = 99;
+// const oldNumberOfShares = 1;
+// const testNewOwner = {};
+// testNewOwner[ownerFieldToModify] = newNumberOfShares;
 
-const nameMap = {};
-nameMap[testOwner] = 'firstName';
-nameMap[testProjectGroup] = 'name';
-nameMap[testSubscriberBill] = 'balance';
-nameMap[testSolarProject] = 'name';
+// const nameMap = {};
+// nameMap[testOwner] = 'firstName';
+// nameMap[testProjectGroup] = 'name';
+// nameMap[testSubscriberBill] = 'balance';
+// nameMap[testSolarProject] = 'name';
 
-const expectedResultsMap = {};
-expectedResultsMap[testOwner] = 'Test';
-expectedResultsMap[testProjectGroup] = 'SAMPLE PROJECT GROUP';
-expectedResultsMap[testSubscriberBill] = 1996;
-expectedResultsMap[testSolarProject] = 'SAMPLE SOLAR PROJECT';
+// const expectedResultsMap = {};
+// expectedResultsMap[testOwner] = 'Test';
+// expectedResultsMap[testProjectGroup] = 'SAMPLE PROJECT GROUP';
+// expectedResultsMap[testSubscriberBill] = 1996;
+// expectedResultsMap[testSolarProject] = 'SAMPLE SOLAR PROJECT';
 
-// const testPersonToCreate = {
-//   email: 'new_person@pppower.com',
-//   phoneNumber: '1111111111',
-//   name: 'NEW PERSON',
-//   street: 'NEW PERSON STREET',
-//   city: 'NEW PERSON CITY',
-//   state: 'NEW PERSON STATE',
-//   apt: 'NEW PERSON APT',
-//   zipcode: 'NEW PERSON ZIPCODE',
-//   onboardingStep: 6,
-//   mailingStreet: 'NEW PERSON MAILING STREET',
-//   mailingApt: 'NEW PERSON MAILING APT',
-//   mailingCity: 'NEW PERSON MAILING CITY',
-//   mailingState: 'NEW PERSON MAILING STATE',
-//   mailingZipcode: 'NEW PERSON MAILING ZIPCODE'
+// // const testPersonToCreate = {
+// //   email: 'new_person@pppower.com',
+// //   phoneNumber: '1111111111',
+// //   name: 'NEW PERSON',
+// //   street: 'NEW PERSON STREET',
+// //   city: 'NEW PERSON CITY',
+// //   state: 'NEW PERSON STATE',
+// //   apt: 'NEW PERSON APT',
+// //   zipcode: 'NEW PERSON ZIPCODE',
+// //   onboardingStep: 6,
+// //   mailingStreet: 'NEW PERSON MAILING STREET',
+// //   mailingApt: 'NEW PERSON MAILING APT',
+// //   mailingCity: 'NEW PERSON MAILING CITY',
+// //   mailingState: 'NEW PERSON MAILING STATE',
+// //   mailingZipcode: 'NEW PERSON MAILING ZIPCODE'
+// // };
+
+// const testOwnerToCreate = {
+//   ownerTypes: ['General', 'Subscriber']
 // };
 
-const testOwnerToCreate = {
-  ownerTypes: ['General', 'Subscriber']
-};
+// // const testUserLoginToCreate = {
+// //   email: 'new_person@pppower.com',
+// //   password: 'apple_jacks'
+// // };
 
-// const testUserLoginToCreate = {
-//   email: 'new_person@pppower.com',
-//   password: 'apple_jacks'
-// };
+// // the describe(...) clause the function being tested
+// // the test(...) claus describes the expected behavior
 
-// the describe(...) clause the function being tested
-// the test(...) claus describes the expected behavior
+// describe('getAllProjectGroups function', () => {
+//   test('expect list of all project groups', async () => {
+//     const res = await getAllProjectGroups();
+//     expect(res.length).not.toBe(undefined);
+//   });
+// });
 
-describe('getAllProjectGroups function', () => {
-  test('expect list of all project groups', async () => {
-    const res = await getAllProjectGroups();
-    expect(res.length).not.toBe(undefined);
-  });
-});
+// describe('getAnnouncementsByProjectGroup function', () => {
+//   test('expect list of announcements', async () => {
+//     const res = await getAnnouncementsByProjectGroupId(testProjectGroup);
+//     expect(res.length).not.toBe(undefined);
+//   });
+// });
 
-describe('getAnnouncementsByProjectGroup function', () => {
-  test('expect list of announcements', async () => {
-    const res = await getAnnouncementsByProjectGroupId(testProjectGroup);
-    expect(res.length).not.toBe(undefined);
-  });
-});
+// describe('getOwnersByEmail function', () => {
+//   test('expect list of users', async () => {
+//     const res = await getOwnersByEmail(testEmail);
+//     expect(res.length).not.toBe(undefined);
+//   });
+// });
 
-describe('getOwnersByEmail function', () => {
-  test('expect list of users', async () => {
-    const res = await getOwnersByEmail(testEmail);
-    expect(res.length).not.toBe(undefined);
-  });
-});
-
-describe('getOwnerById function', () => {
-  test('expect owner record', async () => {
-    const res = await getOwnerById(testOwner);
-    expect(res).not.toBe(undefined);
-
-    const key = nameMap[testOwner];
-    const value = expectedResultsMap[testOwner];
-    expect(res[key]).toStrictEqual(value);
-  });
-});
-
-describe('getProjectGroupById function', () => {
-  test('expect project group record', async () => {
-    const res = await getProjectGroupById(testProjectGroup);
-    expect(res).not.toBe(undefined);
-
-    const key = nameMap[testProjectGroup];
-    const value = expectedResultsMap[testProjectGroup];
-    expect(res[key]).toStrictEqual(value);
-  });
-});
-
-// describe('getSubscriberBillById function', () => {
-//   test('expect subscriber bill record', async () => {
-//     const res = await getSubscriberBillById(testSubscriberBill);
+// describe('getOwnerById function', () => {
+//   test('expect owner record', async () => {
+//     const res = await getOwnerById(testOwner);
 //     expect(res).not.toBe(undefined);
 
-//     const key = nameMap[testSubscriberBill];
-//     const value = expectedResultsMap[testSubscriberBill];
+//     const key = nameMap[testOwner];
+//     const value = expectedResultsMap[testOwner];
 //     expect(res[key]).toStrictEqual(value);
 //   });
 // });
 
-describe('getSolarProjectById function', () => {
-  test('expect solar project record', async () => {
-    const res = await getSolarProjectById(testSolarProject);
-    expect(res).not.toBe(undefined);
+// describe('getProjectGroupById function', () => {
+//   test('expect project group record', async () => {
+//     const res = await getProjectGroupById(testProjectGroup);
+//     expect(res).not.toBe(undefined);
 
-    const key = nameMap[testSolarProject];
-    const value = expectedResultsMap[testSolarProject];
-    expect(res[key]).toStrictEqual(value);
-  });
-});
-
-// describe('updateSubscriberBill function', () => {
-//   test('expect id of updated bill', async () => {
-//     let res = await updateSubscriberBill(
-//       testSubscriberBill,
-//       testNewSubscriberBill
-//     );
-//     expect(res).toBe(testSubscriberBill);
-
-//     const testOldBill = {};
-//     testOldBill[billFieldToModify] = oldBalance;
-
-//     res = await updateSubscriberBill(testSubscriberBill, testOldBill);
-//     expect(res).toBe(testSubscriberBill);
+//     const key = nameMap[testProjectGroup];
+//     const value = expectedResultsMap[testProjectGroup];
+//     expect(res[key]).toStrictEqual(value);
 //   });
 // });
 
-describe('updateOwner function', () => {
-  test('expect id of updated owner', async () => {
-    let res = await updateOwner(testOwner, testNewOwner);
-    expect(res).toBe(testOwner);
-    const testOldOwner = {};
-    testOldOwner[ownerFieldToModify] = oldNumberOfShares;
+// // describe('getSubscriberBillById function', () => {
+// //   test('expect subscriber bill record', async () => {
+// //     const res = await getSubscriberBillById(testSubscriberBill);
+// //     expect(res).not.toBe(undefined);
 
-    res = await updateOwner(testOwner, testOldOwner);
-    expect(res).toBe(testOwner);
-  });
-});
+// //     const key = nameMap[testSubscriberBill];
+// //     const value = expectedResultsMap[testSubscriberBill];
+// //     expect(res[key]).toStrictEqual(value);
+// //   });
+// // });
 
-describe('createOwner/deleteOwner function', () => {
-  test('expect id of created owner and {} after deletion', async () => {
-    // create owner
-    const id = await createOwner(testOwnerToCreate);
-    expect(id).not.toBe(null);
+// describe('getSolarProjectById function', () => {
+//   test('expect solar project record', async () => {
+//     const res = await getSolarProjectById(testSolarProject);
+//     expect(res).not.toBe(undefined);
 
-    // delete owner
-    const res = await deleteOwner(id);
-    expect(res).toStrictEqual({});
-  });
-});
+//     const key = nameMap[testSolarProject];
+//     const value = expectedResultsMap[testSolarProject];
+//     expect(res[key]).toStrictEqual(value);
+//   });
+// });
+
+// // describe('updateSubscriberBill function', () => {
+// //   test('expect id of updated bill', async () => {
+// //     let res = await updateSubscriberBill(
+// //       testSubscriberBill,
+// //       testNewSubscriberBill
+// //     );
+// //     expect(res).toBe(testSubscriberBill);
+
+// //     const testOldBill = {};
+// //     testOldBill[billFieldToModify] = oldBalance;
+
+// //     res = await updateSubscriberBill(testSubscriberBill, testOldBill);
+// //     expect(res).toBe(testSubscriberBill);
+// //   });
+// // });
+
+// describe('updateOwner function', () => {
+//   test('expect id of updated owner', async () => {
+//     let res = await updateOwner(testOwner, testNewOwner);
+//     expect(res).toBe(testOwner);
+//     const testOldOwner = {};
+//     testOldOwner[ownerFieldToModify] = oldNumberOfShares;
+
+//     res = await updateOwner(testOwner, testOldOwner);
+//     expect(res).toBe(testOwner);
+//   });
+// });
+
+// describe('createOwner/deleteOwner function', () => {
+//   test('expect id of created owner and {} after deletion', async () => {
+//     // create owner
+//     const id = await createOwner(testOwnerToCreate);
+//     expect(id).not.toBe(null);
+
+//     // delete owner
+//     const res = await deleteOwner(id);
+//     expect(res).toStrictEqual({});
+//   });
+// });
